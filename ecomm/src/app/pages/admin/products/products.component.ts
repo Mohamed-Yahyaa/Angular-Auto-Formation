9d64445ec1e05699df9c9cae1d2e8d46990e384c
Mohamed yahya <92155140+Mohamed-Yahyaa@users.noreply.github.com>
2024-01-23 17:59:17 +0100

    add

--- ecomm/src/app/pages/admin/products/products.component.ts
+++ ecomm/src/app/pages/admin/products/products.component.ts
@@ -1,12 +1,38 @@
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './products.component.html',
   styleUrl: './products.component.scss'
 })
 export class ProductsComponent {
+
+  Panel: boolean = false;
+
+  PanelNew(){
+    this.Panel = true;
+  }
+
+  PanelClose(){
+    this.Panel =  false;
+
+  }
+
+  productObj: any ={
+    "productId" : 1,
+    "productName": '',
+    "productPrice":0,
+    "createdDate":  new Date(),
+    "categoryId": 0
+
+  }
+  CategoryList: any [] = [];
+  // constructor(private productSrv: ProductService){
+
+  // }
 
 }
